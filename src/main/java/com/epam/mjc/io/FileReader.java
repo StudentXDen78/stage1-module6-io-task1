b8e2fc904d9f4f80b3e3e688c87266a8c5247ece
xden78 <venera.opt@gmail.com>
2024-10-28 21:43:00 +0300

    3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,27 +2,25 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 
 public class FileReader {
     public static Profile getDataFromFile(File file)
     {
-        String path = "C:\\Users\\Admin\\IdeaProjects\\stage1-module6-io-task1\\src\\main\\resources\\";
-        String absolutePath = path + file;
+        // String path = "C:\\Users\\Admin\\IdeaProjects\\stage1-module6-io-task1\\src\\main\\resources\\";
+        // File absolutePath = new File(file.toString());
         StringBuilder dataFromFile = new StringBuilder();
-        try(FileInputStream fileInputStream = new FileInputStream(absolutePath)) {
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
             int ch;
             while((ch = fileInputStream.read()) != -1) {
                 dataFromFile.append((char)ch);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } catch (IOException a) {
-            System.out.println(a.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
 
         HashMap<String, String> dataInLibraryMap= new HashMap<>();
@@ -33,12 +31,11 @@ public class FileReader {
             dataInLibraryMap.put(pairs[0], pairs[1]);
         }
 
-        for (String key : dataInLibraryMap.keySet()){
-            System.out.println(key + " : " + dataInLibraryMap.get(key));
-            if (Objects.equals(key, "Name")) anna.setName(dataInLibraryMap.get(key));
-            if (Objects.equals(key, "Age")) anna.setAge(Integer.parseInt(dataInLibraryMap.get(key)));
-            if (Objects.equals(key, "Email")) anna.setEmail(dataInLibraryMap.get(key));
-            if (Objects.equals(key, "Phone")) anna.setPhone(Long.parseLong(dataInLibraryMap.get(key)));
+        for (Map.Entry<String, String> key : dataInLibraryMap.entrySet()){
+            if (Objects.equals(key.getKey(), "Name")) anna.setName(dataInLibraryMap.get(key.getKey()));
+            if (Objects.equals(key.getKey(), "Age")) anna.setAge(Integer.parseInt(dataInLibraryMap.get(key.getKey())));
+            if (Objects.equals(key.getKey(), "Email")) anna.setEmail(dataInLibraryMap.get(key.getKey()));
+            if (Objects.equals(key.getKey(), "Phone")) anna.setPhone(Long.parseLong(dataInLibraryMap.get(key.getKey())));
         }
 
         return anna;
